a683d5e02e5ef2c68f2fc3ffe30cfb51db8933b7
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-04-20 18:41:02 -0500

    Final changes in css and refactoring

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,10 +4,21 @@ import './Pokedex.css'
 
 class Pokedex extends React.Component {
     render() {
+        let title;
+        let score;
+        if(this.props.isWinner) {
+            title = <h1 className="Pokedex-winner"> Winner Hand</h1>
+            score = <h2 className="Pokedex-winner">Exp: {this.props.score}</h2>;
+        } else {
+            title = <h1 className="Pokedex-loser"> Loser Hand</h1>
+            score = <h2 className="Pokedex-loser">Exp:{this.props.score}</h2>;
+        }
+
+        
         return (
             <div className="Pokedex">
-                <h1 className={this.props.isWinner ? 'Pokedex-winner': 'Pokedex-loser'}>{this.props.isWinner ? 'Winner Hand':'Loser Hand'}</h1>
-                <h2 className={this.props.isWinner ? 'Pokedex-winner': 'Pokedex-loser'}>{this.props.score}</h2>
+                {title}
+                {score}
                 <div className="Pokedex-container">
                     {this.props.pokemon.map(p => (
                     <Pokecard key={p.id} id = {p.id} name = {p.name} type = {p.type} base_experience = {p.base_experience} />
