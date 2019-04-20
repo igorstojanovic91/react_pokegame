feb50bd0ab2c44846cbe636e6156a66d6d38fdda
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-04-20 14:35:02 -0500

    Setting up file structure for game

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Pokedex />
+        <Pokegame />
       </div>
     );
   }
---
+++ src/Pokegame.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Pokedex from './Pokedex'
+
+class Pokedex extends React.Component {
+    render() {
+        return (
+            
+        )
+    }
+}
